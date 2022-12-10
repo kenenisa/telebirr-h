ea0dab76a4435c8dc30efc92fca73fa20b4a9d2b
kenenisa <kenenisaalemayhu0@gmail.com>
2022-12-10 18:02:15 +0300

    asdfa

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Splash();
+    return Home();
   }
 }
